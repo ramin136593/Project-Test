fd6bd432b4f3ba7de8ff588d9750385b55da6923
Ramin <rm.shams93@gmail.com>
2023-10-31 20:01:02 +0330

    index and contact page add

--- MyProject_Mvc/Controllers/HomeController.cs
+++ MyProject_Mvc/Controllers/HomeController.cs
@@ -1,32 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using MyProject_Mvc.Models;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MyProject_Mvc.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+           
         }
+        private readonly List<Service> _services = new List<Service>
+        {
+            new Service(1, "نقره ای"),
+            new Service(2, "طلایی"),
+            new Service(3, "پلاتین"),
+            new Service(4, "الماس"),
+        };
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
-            return View();
+            var model = new Contact
+            {
+                Services = new SelectList(_services, "Id", "Name")
+            };
+            return View(model);
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
